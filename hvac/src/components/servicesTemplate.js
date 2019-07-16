fbc0a8facbb3838bac50607794c276f144b773c5
brutchjd <brutchjd@rose-hulman.edu>
2019-07-16 02:46:18 -0400

    Fixed more css with bootstrap, made the image on the services pages into a figure with a caption

--- hvac/src/components/servicesTemplate.js
+++ hvac/src/components/servicesTemplate.js
@@ -1,33 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Jumbotron, Container, Row, Col, Image, Button, Figure } from 'react-bootstrap'
+import { Jumbotron, Container, Row, Col, Button, Figure } from 'react-bootstrap'
 import './servicesTemplate.css'
 import IRMbutton from './subcomponents/IRMbuttons'
 
 function Services(props) {
-    var h2Class = "text-center text-light font-weight-bold image-cap rounded" + " bg-" + props.color;
+    var captionClass = "text-center text-light font-weight-bold image-cap" + " bg-" + props.color;
     return(
         <Container fluid="true" className="mt-3 overflow-hidden">
-            {/* <Row>
-                <Image src={props.imgsrc} className="img-fluid w-100 overflow-hidden" ></Image>
-                <Col xs={{ span: 5, offset: 6 }}>
-                    <h2 className={h2Class} >{props.title}</h2>
-                </Col>
-            </Row> */}
             <Row>
                 <Figure>
                     <Figure.Image
                         fluid={true}
                         src={props.imgsrc}
                     />
-                    <Figure.Caption className="text-center ">
+                    <Figure.Caption className={captionClass}>
                         {props.title}
                     </Figure.Caption>
                 </Figure>
             </Row>
             <Row>
                 <Col>
-                    <Jumbotron className="rounded p-10 mt-3 mb-0">
+                    <Jumbotron className="rounded mt-3 mb-0">
                         <h4 className="font-weight-bold">We do both commercial and residential!</h4>
                         <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis ut diam quam nulla porttitor. Pick up snacks at the concession stand today! Half price popcorn for all kids under the age of 12. We now have cinamon pretzels with frosting on top of our original salty pretzel with cheese. I am trying to make a third line of words. Almost there. Cool.</h5>
                     </Jumbotron>
