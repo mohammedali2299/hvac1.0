e47f7959ca1091543e221d998f3a85ec3f7e3de5
Mohammed Ali <alima@alima-5520.rose-hulman.edu>
2019-07-23 03:35:36 -0400

    reformatted the home page, added servive links on top, removed res/com tabs

--- hvac/src/components/Home.js
+++ hvac/src/components/Home.js
@@ -40,10 +40,53 @@ function Home() {
                         </Row>
                     </Col>
                     <Col xs={12} lg={8} className="mt-3">
-                        <ResComTab />
+                        {/* <ResComTab /> */}
+                        <Row className="mt-3" noGutters>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={6} className="pr-2">
+                        <div className="hovereffect">
+                            <img className="img-responsive w-100 h-100" src="/extras/home-cooling.jpg" alt=""/>
+                            <div className="overlay">
+                                <h2 className="cooling">Cooling</h2>
+                                <p className="info">Need a proffessional to take a look? We service all makes and models of AC units.</p>
+                                <a href="/cooling" className="info">Learn More</a>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                        <div className="hovereffect">
+                            <img className="img-responsive w-100 h-100" src="/extras/home-heating.jpg" alt=""/>
+                            <div className="overlay">
+                                <h2 className="heating">Heating</h2>
+                                <p className="info">Heat doesn't seem to work? Fill out the contact form and we'll get back to you!</p>
+                                <a href="/heating" className="info">Learn More</a>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={6} className="mt-2 pr-2">
+                        <div className="hovereffect">
+                            <img className="img-responsive w-100 h-100" src="/extras/home-electrical.jpg" alt=""/>
+                            <div className="overlay">
+                                <h2 className="electrical">Electrical</h2>
+                                <p className="info">As certified electricians, we can handle any electrical problem you have.</p>
+                                <a href="/electrical" className="info">Learn More</a>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={6} className="mt-2">
+                        <div class="hovereffect">
+                            <img className="img-responsive w-100 h-100" src="/extras/home-ducting.jpg" alt=""/>
+                            <div className="overlay">
+                                <h2 className="ducting">Ducting</h2>
+                                <p className="info">Ducts leaking air? We're experts in installing and sealing ducts and we would
+                                love to help.</p>
+                                <a href="/ducting" className="info">Learn More</a>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
-                <Row className="mt-3">
+                </Col>
+                </Row>
+                {/* <Row className="mt-3">
                     <Col xs={12} sm={12} md={6} lg={6} xl={3}>
                         <div className="hovereffect">
                             <img className="img-responsive w-100 h-100" src="/extras/home-cooling.jpg" alt=""/>
@@ -64,7 +107,7 @@ function Home() {
                             </div>
                         </div>
                     </Col>
-                    <Col xs={12} sm={12} md={6} lg={6} xl={3}>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={3} className="mt-2">
                         <div className="hovereffect">
                             <img className="img-responsive w-100 h-100" src="/extras/home-electrical.jpg" alt=""/>
                             <div className="overlay">
@@ -74,7 +117,7 @@ function Home() {
                             </div>
                         </div>
                     </Col>
-                    <Col xs={12} sm={12} md={6} lg={6} xl={3}>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={3} className="mt-2">
                         <div class="hovereffect">
                             <img className="img-responsive w-100 h-100" src="/extras/home-ducting.jpg" alt=""/>
                             <div className="overlay">
@@ -85,7 +128,7 @@ function Home() {
                             </div>
                         </div>
                     </Col>
-                </Row>
+                </Row> */}
                 <Row className="mt-3">
                     <ReviewCarousel/>
                 </Row>
--- hvac/src/components/subcomponents/ReviewCarousel.js
+++ hvac/src/components/subcomponents/ReviewCarousel.js
@@ -30,9 +30,9 @@ class ReviewCarousel extends React.Component {
         <Carousel activeIndex={index} direction={direction} onSelect={this.handleSelect} fade={true}>
           <Carousel.Item>
             <img className="reviewimg" src="/extras/review3.jpg" alt="First slide"/>
-            <Carousel.Caption className="mb-5">
+            <Carousel.Caption>
                 <h3>Satisfied Customer</h3>
-                <p className="mt-3 font-italic">"This is truly an all-inclusive HVAC and electrical solution provider. 
+                <p className="font-italic">"This is truly an all-inclusive HVAC and electrical solution provider. 
                   They just don't throw parts or fix issues, they provide solutions with guidance. 
                   It has been a true pleasure to have this company work at my home. 
                   I had my Furnace, Humidifier, Air Conditioner and Thermostat changed. 
@@ -40,7 +40,7 @@ class ReviewCarousel extends React.Component {
                   at a very reasonable cost (trust me I shopped). I saw significant savings in my utility bills. 
                   I don't usually put in ratings and reviews but did take my time to put this in because these guys 
                   are truly great."</p>
-                <p className="mt-3">Click here to see more 
+                <p>Click here to see more 
                 <a className="font-weight-bold" href="https://www.google.com/search?q=google+reviews+united+hvac+west+chicago&rlz=1C1GCEB_enUS760US760&oq=goog&aqs=chrome.0.69i59j0j69i60j69i57j0l2.1252j0j4&sourceid=chrome&ie=UTF-8#lrd=0x880eff0af146c8bb:0x486eef7a5d48d4b6,1,,,"> customer reviews</a></p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -56,16 +56,16 @@ class ReviewCarousel extends React.Component {
           </Carousel.Item>
           <Carousel.Item>
             <img className="reviewimg" src="/extras/review3.jpg" alt="Third slide"/>
-            <Carousel.Caption className="mb-5">
+            <Carousel.Caption className="mb-2">
               <h3>Satisfied Customer</h3>
-              <p className="mt-3 font-italic">"Imran was not only the first one to respond to my calls but was at my place immediately 
+              <p className="font-italic">"Imran was not only the first one to respond to my calls but was at my place immediately 
                   as I was having a furnace emergency on the coldest day of the year.  
                   His dedication to his work and level of quality in his service is commendable.  
                   He knows his stuff and will get to the root of the problem to permanently fix it.  
                   A bonus is that he's so much more affordable than others I called.  
                   He's reliable and very professional. Would recommend him any day!""
               </p>
-              <p className="mt-3">Click here to see more 
+              <p>Click here to see more 
             <a className="font-weight-bold" href="https://www.google.com/search?q=google+reviews+united+hvac+west+chicago&rlz=1C1GCEB_enUS760US760&oq=goog&aqs=chrome.0.69i59j0j69i60j69i57j0l2.1252j0j4&sourceid=chrome&ie=UTF-8#lrd=0x880eff0af146c8bb:0x486eef7a5d48d4b6,1,,,"> customer reviews</a></p>
             </Carousel.Caption>
           </Carousel.Item>
