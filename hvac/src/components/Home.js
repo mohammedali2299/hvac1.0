537b02c408a91cdda8397a12e18d6d4cba825485
Mohammed Ali <alima@alima-5520.rose-hulman.edu>
2019-07-24 09:49:55 -0400

    modified home a bit

--- hvac/src/components/Home.js
+++ hvac/src/components/Home.js
@@ -4,6 +4,7 @@ import './Home.css'
 import ResComTab from './subcomponents/ResComTabs'
 import ReviewCarousel from './subcomponents/ReviewCarousel'
 import ServiceLinks from './subcomponents/ServiceLinks'
+import ContactLink from './subcomponents/ContactLink'
 import SocialMedia from './subcomponents/SocialMedia'
 import { cpus } from 'os';
 
