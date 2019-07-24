08f900531b3dcc37ab1d6d2d7c0e34f81f633686
brutchjd <brutchjd@rose-hulman.edu>
2019-07-24 05:00:45 -0400

    merge to get mdbootsrap icons for social media.

--- hvac/src/components/subcomponents/ServiceLinks.js
+++ hvac/src/components/subcomponents/ServiceLinks.js
@@ -12,21 +12,21 @@ function ServiceLinks() {
                 </Col>
                 <Col xs={12} md={6} xl={{ span: 3, offset: 1}}>
                     <div className="hovereffect border">
-                         <img className="img-responsive w-100 h-100" src="/extras/home-cooling.jpg" alt=""/>
-                         <div className="overlay">
-                             <h2 className="cooling">Cooling</h2>
-                             <p className="info">Need a proffessional to take a look? We service all makes and models of AC units.</p>
-                             <a href="/cooling" className="info">Learn More</a>
-                         </div>
+                        <img className="img-responsive w-100 h-100" src="/extras/home-cooling.jpg" alt=""/>
+                        <div className="overlay">
+                            <h2 className="cooling">Cooling</h2>
+                            <p className="info">Need a proffessional to take a look? We service all makes and models of AC units.</p>
+                            <a href="/cooling" className="info">Learn More</a>
+                        </div>
                      </div>
                 </Col>
                 <Col xs={12} md={6} xl={3}>
                     <div className="hovereffect border">
                          <img className="img-responsive w-100 h-100" src="/extras/home-heating.jpg" alt=""/>
                          <div className="overlay">
-                             <h2 className="heating">Heating</h2>
-                             <p className="info">Heat doesn't seem to work? Fill out the contact form and we'll get back to you!</p>
-                             <a href="/heating" className="info">Learn More</a>
+                            <h2 className="heating">Heating</h2>
+                            <p className="info">Heat doesn't seem to work? Fill out the contact form and we'll get back to you!</p>
+                            <a href="/heating" className="info">Learn More</a>
                          </div>
                      </div>
                 </Col>
@@ -43,9 +43,9 @@ function ServiceLinks() {
                     <div className="hovereffect border">
                          <img className="img-responsive w-100 h-100" src="/extras/home-electrical.jpg" alt=""/>
                          <div className="overlay">
-                             <h2 className="electrical">Electrical</h2>
+                            <h2 className="electrical">Electrical</h2>
                             <p className="info">As certified electricians, we can handle any electrical problem you have.</p>
-                             <a href="/electrical" className="info">Learn More</a>
+                            <a href="/electrical" className="info">Learn More</a>
                          </div>
                      </div>
                 </Col>
@@ -53,10 +53,10 @@ function ServiceLinks() {
                     <div class="hovereffect border">
                          <img className="img-responsive w-100 h-100" src="/extras/home-ducting.jpg" alt=""/>
                          <div className="overlay">
-                             <h2 className="ducting">Ducting</h2>
-                             <p className="info">Ducts leaking air? We're experts in installing and sealing ducts and we would
-                             love to help.</p>
-                             <a href="/ducting" className="info">Learn More</a>
+                            <h2 className="ducting">Ducting</h2>
+                            <p className="info">Ducts leaking air? We're experts in installing and sealing ducts and we would
+                            love to help.</p>
+                            <a href="/ducting" className="info">Learn More</a>
                          </div>
                      </div>
                 </Col>
