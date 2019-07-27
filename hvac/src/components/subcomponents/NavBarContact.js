f361ef052c69955eec8bb8fd9dcc38f1e1da6bce
brutchjd <brutchjd@rose-hulman.edu>
2019-07-27 19:56:42 -0400

    Editing navbar, screenshots of website attached

--- hvac/src/components/subcomponents/NavBarContact.js
+++ hvac/src/components/subcomponents/NavBarContact.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Row, Col } from 'react-bootstrap';
 
 function NavBarContact() {
     return (
-        <a href='/contact'>
-            <Image src="/extras/contactnavlink.jpg" className="w-25 h-25"></Image>
-        </a>
-        
+        <Row fluid noGutters className="pt-3">
+            <Col xs={12}>
+                <h6>Phone: <a href="/contact">(847)-489-4796</a></h6>
+            </Col>
+            <Col xs={12}>
+                <h6>Email: <a className="yellow-text" href="mailto:unitedhvac@me.com">unitedhvac@me.com</a></h6>
+            </Col>
+            <Col xs={12}>
+                <h6>Social Media: Coming Soon!</h6>
+            </Col>
+        </Row>
     );
 }
 
