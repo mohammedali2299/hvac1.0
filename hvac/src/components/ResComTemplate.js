5aebd475749a831aeab561ae3234b4bb80968f7f
brutchjd <brutchjd@rose-hulman.edu>
2019-07-30 01:02:43 -0400

    finished the carousel for home, need to make the component now change style with props.

--- hvac/src/components/ResComTemplate.js
+++ hvac/src/components/ResComTemplate.js
@@ -15,18 +15,17 @@ function ResComTemplate(props) {
 
     
     return(
-        <Container className="body">
-            <Row>
-                <Col xs={12} lg={6} className="mt-4">
-                    <Jumbotron fluid className="p-0 bg-white shadow-element">
+        <Container fluid className="body m-0 pl-0 pr-0">
+            <Row noGutters fluid className="m-0" >
+                <Col xs={12} lg={12} className="mt-0">
+                    <InteractiveCarousel name={props.name} />
+                </Col>
+                <Col xs={12} lg={12} className="mt-4 mb-4 pl-3 pr-3">
+                    <Jumbotron fluid className="p-0 m-0 bg-white shadow-element">
                         <h2 className="title-color text-white pt-3 pb-3 text-center w-100">{title.toUpperCase()}</h2>
                         <p className="m-3">{mainText}</p>
                         <Button href="/contact" className="button-color shadow-element btn-lg border-0 m-3">Get Started</Button>
                     </Jumbotron>
-                </Col>
-                
-                <Col xs={12} lg={6} className="mt-4">
-                    <InteractiveCarousel styling={props.name} />
                 </Col>
             </Row>
         </Container>
