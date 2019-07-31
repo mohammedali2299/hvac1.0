fd5b7cc4ad580163ecc4750dda32c4b7e0280617
brutchjd <brutchjd@rose-hulman.edu>
2019-07-31 01:58:07 -0400

    began making the email part of the form, installed files in hvac1.0

--- hvac/src/components/subcomponents/ContactForm.js
+++ hvac/src/components/subcomponents/ContactForm.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react"
-import { Jumbotron, Row, Col, Form, Button } from 'react-bootstrap'
+import { Jumbotron, Row, Col, Form, Button, AccordionCollapse } from 'react-bootstrap'
+import axios from 'axios'
 
 class ContactForm extends Component {
-    constructor(props, context) {
-        super(props, context);
+    constructor(props) {
+        super(props);
     
-        var this.title;
 
         if(props.name === "emergency") {
             this.title = "Emergency Contact Form"
@@ -14,44 +14,68 @@ class ContactForm extends Component {
         }
     
         this.state = {
-            index: 0,
-            direction: null,
-
-
+            name: '',
+            phone: '',
+            email: '',
+            rescom: '',
+            services: '',
+            description: '',
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleChange = e => {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+
+
+        const { name, phone, email, rescom, services, description } = this.state;
+
+        const form = await axios.post('api/form', {
+            name,
+            phone,
+            email,
+            rescom,
+            services,
+            description
+        })
+    }
 
 
 
     render() {
         return (
             <Jumbotron fluid className="mb-0 mt-0 pt-0 bg-white shadow-element">
-                <h3 className={props.name + "-text mb-3 pt-2 pb-2 pl-3 pr-5 text-white rounded"}>{this.title}</h3>
-                <Form className="pr-3 pl-3">
+                <h3 className={this.props.name + "-text mb-3 pt-2 pb-2 pl-3 pr-5 text-white rounded"}>{this.title}</h3>
+                <Form onSubmit={this.handleSubmit} className="pr-3 pl-3">
                     <Form.Group as={Row} controlId="name">
                         <Form.Label column xs={2} >Name</Form.Label>
                         <Col xs={10} >
-                            <Form.Control type="text" className={props.name + "-border"} placeholder="First and Last Name" />
+                            <Form.Control type="text" onChange={this.handleChange} className={this.props.name + "-border"} placeholder="First and Last Name" />
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} controlId="phone">
                         <Form.Label column xs={2} >Phone</Form.Label>
                         <Col xs={10} >
-                            <Form.Control type="tel" className={props.name + "-border"} placeholder="xxx-xxx-xxxx"/>
+                            <Form.Control type="tel" onChange={this.handleChange} className={this.props.name + "-border"} placeholder="xxx-xxx-xxxx"/>
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} controlId="email">
                         <Form.Label column xs={2} >Email</Form.Label>
                         <Col xs={10} >
-                            <Form.Control type="email" className={props.name + "-border"} placeholder="example@gmail.com"/>
+                            <Form.Control type="email" onChange={this.handleChange} className={this.props.name + "-border"} placeholder="example@gmail.com"/>
                         </Col>
                     </Form.Group>
-                    <Form.Group as={Row} controlId="rescom d-flex">
+                    <Form.Group as={Row} controlId="rescom">
                         <Form.Check type="radio" label="Residential" className="mr-2 ml-3" name="rescom" id="residential" />
                         <Form.Check type="radio" label="Commercial" name="rescom" id="commercial" /> 
                     </Form.Group>
-                    <Form.Group as={Row} controlId="services d-flex">
+                    <Form.Group as={Row} controlId="services">
                         <Form.Check label="Cooling" name="services" id="cooling" className="ml-3" />
                         <Form.Check label="Heating" name="services" id="heating" className="ml-2" />
                         <Form.Check label="Ducting" name="services" id="ducting" className="ml-2"/>
@@ -60,12 +84,12 @@ class ContactForm extends Component {
                     </Form.Group>
                     <Form.Group as={Row} controlId="description" className="mt-2">
                         <Col xs={{ offset: 0, span: 12 }}  >
-                        <Form.Control as="textarea" className={props.name + "-border"} rows="5" placeholder="Write a description of the issue and we will get back to you as soon as possible." />
+                        <Form.Control as="textarea" onChange={this.handleChange} className={this.props.name + "-border"} rows="5" placeholder="Write a description of the issue and we will get back to you as soon as possible." />
                         </Col>
                     </Form.Group>
                     <Row>
                         <Col xs={{ offset: 0}} >
-                            <Button className={props.name + "-submit border-0 btn-lg shadow"} type="submit">Submit</Button>
+                            <Button className={this.props.name + "-submit border-0 btn-lg shadow"} type="submit">Submit</Button>
                         </Col>
                     </Row>
                 </Form>
