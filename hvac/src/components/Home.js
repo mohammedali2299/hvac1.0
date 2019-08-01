fb647cf6dd02602ce18a10f9f4adb34384a7d22f
brutchjd <brutchjd@rose-hulman.edu>
2019-07-31 22:26:08 -0400

    installed vs code extensions and formatted these pages

--- hvac/src/components/Home.js
+++ hvac/src/components/Home.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Container, Row, Col, Jumbotron, Button } from 'react-bootstrap'
-import './Home.css'
-import ResComTab from './subcomponents/ResComTabs'
-import ReviewCarousel from './subcomponents/ReviewCarousel'
-import ServiceLinks from './subcomponents/ServiceLinks'
-import { cpus } from 'os';
-import ServiceContact from './subcomponents/ServiceContact'
-import Carousel from './subcomponents/InteractiveCarousel'
-import ContactIcon from './subcomponents/ContactIcon'
+import React from "react";
+import { Container, Row, Col, Jumbotron, Button } from "react-bootstrap";
+import "./Home.css";
+import ResComTab from "./subcomponents/ResComTabs";
+import ReviewCarousel from "./subcomponents/ReviewCarousel";
+import ServiceLinks from "./subcomponents/ServiceLinks";
+import { cpus } from "os";
+import ServiceContact from "./subcomponents/ServiceContact";
+import Carousel from "./subcomponents/InteractiveCarousel";
+import ContactIcon from "./subcomponents/ContactIcon";
 
-function Home() {   
-    return(
-        <div className="body">
-            <Container fluid className="pl-0 pr-0">
-                <Row noGutters >
-                    <Col lg={3} >
-                        <ContactIcon />
-                    </Col>
-                    <Col lg={{ span: 9, offset: 0 }} >
-                        <Carousel name="home" />
-                    </Col>
-                </Row>
-                {/* <Row>
+function Home() {
+  return (
+    <div className="body">
+      <Container fluid className="pl-0 pr-0">
+        <Row noGutters>
+          <Col lg={3}>
+            <ContactIcon />
+          </Col>
+          <Col lg={{ span: 9, offset: 0 }}>
+            <Carousel name="home" />
+          </Col>
+        </Row>
+        {/* <Row>
                     <Col className="col-centered pt-5 pb-5 title shadow-element">
                         <h1 className="font-weight-bold text-center text-white mb-4">Why United?</h1>
                         <p className="h5 font-italic text-center text-white mr-5 ml-5">
@@ -31,31 +31,31 @@ function Home() {
                         </p>
                     </Col>
                 </Row> */}
-                <Row noGutters className="mt-3">
-                    <Col xs={12} xl={4} className="pr-3 pl-3 mt-3 mb-3">
-                        <ServiceContact name="home" color="cooling" />
-                    </Col>
-                    <Col xl={{ span: 8, offset: 0}} className="mt-3 mb-3 pr-3 pl-3">
-                        <ServiceLinks />
-                    </Col>    
-                </Row>
-                <Row noGutters className="mb-3">
-                    <Col xs={12} xl={4} className="pr-3 pl-3 mt-3 mb-3">
-                        <ServiceContact name="emergency" color="heating" />
-                    </Col>
-                    <Col xs={12} xl={8} className="pr-3 pl-3 mt-3">
-                        <ResComTab />
-                    </Col>
-                    
-                    {/* <SocialMedia/> */}
-                </Row>
+        <Row noGutters className="mt-3">
+          <Col xs={12} xl={4} className="pr-3 pl-3 mt-3 mb-3">
+            <ServiceContact name="home" color="cooling" />
+          </Col>
+          <Col xl={{ span: 8, offset: 0 }} className="mt-3 mb-3 pr-3 pl-3">
+            <ServiceLinks />
+          </Col>
+        </Row>
+        <Row noGutters className="mb-3">
+          <Col xs={12} xl={4} className="pr-3 pl-3 mt-3 mb-3">
+            <ServiceContact name="emergency" color="heating" />
+          </Col>
+          <Col xs={12} xl={8} className="pr-3 pl-3 mt-3">
+            <ResComTab />
+          </Col>
 
-                <Row className="mt-4">
-                    <ReviewCarousel/>
-                </Row>
-            </Container>
-        </div>
-    );
+          {/* <SocialMedia/> */}
+        </Row>
+
+        <Row className="mt-4">
+          <ReviewCarousel />
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
