6afb84cffb06f685e0a312245b84a780f7920221
brutchjd <brutchjd@rose-hulman.edu>
2019-08-06 01:19:29 -0400

    minor styling updates on home page and rescom page

--- hvac/src/components/Home.js
+++ hvac/src/components/Home.js
@@ -21,21 +21,11 @@ function Home() {
             <Carousel name="home" />
           </Col>
         </Row>
-        {/* <Row>
-                    <Col className="col-centered pt-5 pb-5 title shadow-element">
-                        <h1 className="font-weight-bold text-center text-white mb-4">Why United?</h1>
-                        <p className="h5 font-italic text-center text-white mr-5 ml-5">
-                            United HVAC and Electrical Inc. has been providing quality heating and cooling service in the Chicago Area since it's 
-                            inception in 2006. From installing rooftop units on commercial buildings to home AC repair, 
-                            United exceeds the standards set by industry today and offers a reliable service for all.
-                        </p>
-                    </Col>
-                </Row> */}
         <Row noGutters className="mt-3">
-          <Col xs={12} xl={4} className="pr-3 pl-3 mt-3 mb-3">
+          <Col xs={12} xl={4} className="pr-3 pl-3 mt-3 mb-3 pb-3">
             <ServiceContact name="home" color="cooling" />
           </Col>
-          <Col xl={{ span: 8, offset: 0 }} className="mt-3 mb-3 pr-3 pl-3">
+          <Col xl={{ span: 8, offset: 0 }} className="mb-3 pr-3 pl-3">
             <ServiceLinks />
           </Col>
         </Row>
@@ -46,13 +36,11 @@ function Home() {
           <Col xs={12} xl={8} className="pr-3 pl-3 mt-3">
             <ResComTab />
           </Col>
-
-          {/* <SocialMedia/> */}
         </Row>
 
-        <Row className="mt-4">
+        {/* <Row className="mt-4">
           <ReviewCarousel />
-        </Row>
+        </Row> */}
       </Container>
     </div>
   );
--- hvac/src/components/ResComTemplate.js
+++ hvac/src/components/ResComTemplate.js
@@ -9,10 +9,10 @@ function ResComTemplate(props) {
     var startText;
     
     if(props.name === "residential") {
-        mainText = ".......(Some of this text will be moved to carousel) Keep your home comfortable through United! We can help you choose the ac or furnace that is best suited to your home saving you money on electricty and allowing you to regulate the temperature of your home more effectively. After that we offer a full hvac installation service so no work is required on your part to get a fully functional system. You can also get your electrical or hvac systems serviced by our technicians in no time."
+        mainText = "Keep your home comfortable through United! We can help you choose the ac or furnace that is best suited to your home, saving you money on electricty and allowing you to regulate the temperature of your home more effectively. After that we offer a full hvac installation service so no work is required on your part to get a fully functional system. You can also get your electrical or hvac systems serviced by our technicians in no time."
         startText = "Order our service today, or ask us about a specific service we can do for you by pressing the Get Started button below."
     } else if(props.name === "commercial") {
-        mainText = ".......(Some of this text will be moved to carousel) Get all of your commercial hvac needs all in one place. We service apartments, businesses, and warehouses. We install full air conditioning, heating, ventilation, and electrical systems up to date with the latest technology for brand new buildings. We also repair or replace old or broken air conditioners, furnaces, ducting, or electrical systems in old or new buildings. By ordering our hvac service you are ensuring that your new hvac system is running efficiently and not wasting electricity and money trying to keep your building comfortable."
+        mainText = "Get all of your commercial hvac needs all in one place. We service apartments, businesses, and warehouses. We install full air conditioning, heating, ventilation, and electrical systems up to date with the latest technology for brand new buildings. We also repair or replace old or broken air conditioners, furnaces, ducting, or electrical systems in old or new buildings. By ordering our hvac service you are ensuring that your new hvac system is running efficiently and not wasting electricity and money trying to keep your building comfortable."
         startText = "To find out how we can help you with your hvac issue or to order our services now, click the Get Started button below to contact us online or via email or phone."
     }
 
@@ -20,16 +20,16 @@ function ResComTemplate(props) {
     return(
         <Container fluid className="body m-0 pl-0 pr-0">
             <Row noGutters fluid className="m-0" >
-                <Col xs={{ span: 12, order: 2}} lg={{ span: 12, order: 1 }} className="mt-3 mb-3 pl-5 pr-5">
+                <Col xs={12} className="mt-0">
+                    <InteractiveCarousel name={props.name} />
+                </Col>
+                <Col xs={12} className="mt-3 mb-3 pl-5 pr-5">
                     <Jumbotron fluid className="p-0 m-0 bg-white shadow-element">
                         <h2 className="title-color text-white pr-2 pl-2 pt-3 pb-3 text-center w-100">{title.toUpperCase()}</h2>
                         <p className="mr-5 ml-5 mb-3 mt-4 p-medium">{mainText}</p>
                         <p className="mr-5 ml-5 mb-3 mt-4 p-medium">{startText}</p>
                         <Button href="/contact" className="button-color shadow-element btn-lg border-0 mt-4 mb-5 ml-5">Get Started</Button>
                     </Jumbotron>
-                </Col>
-                <Col xs={{ span: 12, order: 1}} lg={{ span: 12, order: 2 }} className="mt-0">
-                    <InteractiveCarousel name={props.name} />
                 </Col>
             </Row>
         </Container>
