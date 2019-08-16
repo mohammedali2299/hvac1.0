fcacca7dc0b514b98535be3c4fc208fe93f698db
brutchjd <brutchjd@rose-hulman.edu>
2019-08-16 18:59:22 -0400

    reseting git ignore

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
