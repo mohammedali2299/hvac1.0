841ff18bd9c99b85237dbdd1d747d85157693a3b
brutchjd <brutchjd@rose-hulman.edu>
2019-08-19 05:23:45 -0400

    fixed typo in last commit

--- index.js
+++ index.js
@@ -55,10 +55,10 @@ app.post('/api/form', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'hvac/build')));
   // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'hvac/build', 'index.html'));
     });
   }
 
