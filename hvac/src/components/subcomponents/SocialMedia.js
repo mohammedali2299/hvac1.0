cfd71f3a7f020f0dc3c863bf9c044c705bad34fb
brutchjd <brutchjd@rose-hulman.edu>
2019-08-19 04:31:58 -0400

    commented out social media code

--- hvac/src/components/subcomponents/SocialMedia.js
+++ hvac/src/components/subcomponents/SocialMedia.js
@@ -5,63 +5,36 @@ import './SocialMedia.css'
 
 function SocialMedia() {
     return(
-        // <Col xs={12} xl={{ span: 3, offset: 1}}>
-            // <div class="hovereffect3">
-            //     {/* <img class="img-responsive w-100 h-100" src="http://placehold.it/350x200" alt="" /> */}
-            //     <div class="overlay3">
-            //         <h2>Social Media</h2>
-            //         <div class="rotate">
-            //             <p class="group1">
-            //                 <a href="#">
-            //                     <i class="fa fa-twitter"></i>
-            //                 </a>
-            //                 <a href="#">
-            //                     <i class="fa fa-facebook"></i>
-            //                 </a>
-            //             </p>
-            //                 <hr/>
-            //                 <hr/>
-            //             <p class="group2">
-            //                 <a href="#">
-            //                     <i class="fa fa-instagram"></i>
-            //                 </a>
-            //                 <a href="#">
-            //                     <i class="fa fa-dribbble"></i>
-            //                 </a>
-            //             </p>
-            //         </div>
-            //     </div>
-            // </div>
-        // </Col>  
-        
-        <Col xs={12} xl={{ span: 5, offset: 1}}>
-            <div class="hovereffect3">
-                <div class="overlay3">
-                    <h2>Social Media</h2>
-                    <div class="rotate">
-                        <p class="group1">
-                            <a href="#">
-                                <i class="fa fa-twitter"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fa fa-facebook"></i>
-                            </a>
-                        </p>
-                            <hr/>
-                            <hr/>
-                        <p class="group2">
-                            <a href="#">
-                                <i class="fa fa-instagram"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fa fa-dribbble"></i>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </Col>
-
+        // <Col xs={12} xl={{ span: 5, offset: 1}}>
+        //     <div class="hovereffect3">
+        //         <div class="overlay3">
+        //             <h2>Social Media</h2>
+        //             <div class="rotate">
+        //                 <p class="group1">
+        //                     <a href="#">
+        //                         <i class="fa fa-twitter"></i>
+        //                     </a>
+        //                     <a href="#">
+        //                         <i class="fa fa-facebook"></i>
+        //                     </a>
+        //                 </p>
+        //                     <hr/>
+        //                     <hr/>
+        //                 <p class="group2">
+        //                     <a href="#">
+        //                         <i class="fa fa-instagram"></i>
+        //                     </a>
+        //                     <a href="#">
+        //                         <i class="fa fa-dribbble"></i>
+        //                     </a>
+        //                 </p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </Col>
+        <div>
+            
+        </div>
     );
 
 }
